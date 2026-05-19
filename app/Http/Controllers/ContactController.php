cf10029df2ae4120008d884c50d73fe6e4516453
colee615 <94701865+colee615@users.noreply.github.com>
2026-05-19 12:26:29 -0400

    Cambios contactos

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function send(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:160'],
+            'email' => ['required', 'email', 'max:160'],
+            'subject' => ['nullable', 'string', 'max:200'],
+            'message' => ['required', 'string', 'max:5000'],
+        ]);
+
+        $recipient = trim((string) env('CONTACT_FORM_TO', 'atencionalcliente@correos.gob.bo'));
+        $subject = trim((string) ($data['subject'] ?? ''));
+
+        Mail::raw($this->buildBody($data), function ($mail) use ($recipient, $data, $subject) {
+            $mail->to($recipient)
+                ->replyTo($data['email'], $data['name'])
+                ->subject($subject !== '' ? $subject : 'Nuevo mensaje de contacto desde la web');
+        });
+
+        return response()->json([
+            'message' => 'Tu mensaje fue enviado correctamente.',
+        ]);
+    }
+
+    protected function buildBody(array $data): string
+    {
+        return implode("\n", [
+            'Nuevo mensaje enviado desde el formulario de contacto.',
+            '',
+            'Nombre: ' . $data['name'],
+            'Correo: ' . $data['email'],
+            'Asunto: ' . (($data['subject'] ?? '') !== '' ? $data['subject'] : 'Sin asunto'),
+            '',
+            'Mensaje:',
+            $data['message'],
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\SitePageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/contact', [ContactController::class, 'send']);
 Route::get('/site/pages/{slug}', [SitePageController::class, 'publicShow']);
